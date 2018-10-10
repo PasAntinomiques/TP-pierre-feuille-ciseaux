2090ddc39875787807c71be6726d824e88aba6d4
Poposs <suprem3842@gmail.com>
2018-10-10 10:45:01 +0200

    core game

--- TP/Pierre_feuille_ciseaux/src/main/Game.java
+++ TP/Pierre_feuille_ciseaux/src/main/Game.java
@@ -1,7 +1,19 @@
 package main;
 
 public class Game {
+	
 	public void playOneTurn() {
+		Player player1 = new Player();
+		Player player2 = new Player();
 		
+		Shape s1 = player1.play();
+		Shape s2 = player2.play();
+		if (s1.compareShape(s2)) {
+//			givePoints(cmd)
+		}
+	}
+	
+	public int givePoints(int cmp) {
+		return 0;
 	}
 }
