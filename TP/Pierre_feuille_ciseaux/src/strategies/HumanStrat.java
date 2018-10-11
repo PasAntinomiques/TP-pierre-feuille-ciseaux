65918c46ac536560cfb20211ea2e7f76296299d6
Poposs <suprem3842@gmail.com>
2018-10-11 08:53:15 +0200

    HumanStrategy

--- TP/Pierre_feuille_ciseaux/src/strategies/HumanStrat.java
+++ TP/Pierre_feuille_ciseaux/src/strategies/HumanStrat.java
@@ -1,13 +1,22 @@
 package strategies;
 
+import java.io.IOException;
+
 import main.Shape;
+import util.Input;
 
 public class HumanStrat implements Strategy {
 
 	@Override
 	public Shape chooseShape() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("Veuillez saisir un entier appartenant à [1, 3]");
+		int answer = 0;
+		try {
+			answer = Input.readInt();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Shape.fromInt(answer);
 	}
 
 	@Override
